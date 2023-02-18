1b49da89708089ba72a6919546a84ef321c7b49f
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-18 11:32:05 +0430

    Handaling the get and resoponse the get request

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/",function(request,response){
     // console.log(request)
-    response.send("Hello Hasibullah")
+    response.send('<h1>Hello World!</h1>')
 })
 
 app.listen(3000,function(){
