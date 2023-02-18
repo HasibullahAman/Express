53f838951015c6c9e321340be07b13c8f336b465
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-18 16:32:43 +0430

    cal-1

--- calculater/claculater.js
+++ calculater/claculater.js
@@ -7,6 +7,9 @@ app.get('/', function(req, res){
     // res.send('<h1>Hello World</h1>');
     res.sendFile(__dirname + '/index.html')
 })
+app.post('/',function(req,res){
+    res.send("Thanks for tuppppp!")
+})
 
 app.listen(3000,function(){
     console.log('Express server listening on port:3000')
