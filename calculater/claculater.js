2a70c4fd5a987d0eb4e1fb8bd3c7dcfb55bc4cdc
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-18 16:17:40 +0430

    create html file for respont

--- calculater/claculater.js
+++ calculater/claculater.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello World</h1>');
+    // res.send('<h1>Hello World</h1>');
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3000,function(){
