4a4f569f30f7f1721e644a55c98239838b8177a8
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-18 11:21:34 +0430

    get_response

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
+
+app.get("/",function(request,response){
+    console.log(request)
+})
 
 app.listen(3000,function(){
     console.log('Hello from lissten server port: 3000')
