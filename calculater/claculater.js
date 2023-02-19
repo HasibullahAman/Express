6474cfa1be03e7cedf59290ed0bec8f5de4e404c
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-19 06:41:20 +0430

    solve-problom

--- calculater/claculater.js
+++ calculater/claculater.js
@@ -1,4 +1,4 @@
-const {req,res} = require('express');
+// const {req,res} = require('express');
 const express = require('express');
 
 const app = express();
@@ -7,9 +7,9 @@ app.get('/', function(req, res){
     // res.send('<h1>Hello World</h1>');
     res.sendFile(__dirname + '/index.html')
 })
-app.post('/',function(req,res){
-    res.send("Thanks for tuppppp!")
-})
+// app.post('/',function(req,res){
+//     res.send("Thanks for tuppppp!")
+// })
 
 app.listen(3000,function(){
     console.log('Express server listening on port:3000')
