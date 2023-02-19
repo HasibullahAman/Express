88e44796595f721baa13d5d71da65a35b6ec8133
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-19 07:09:55 +0430

    add_cacluator created

--- calculater/claculater.js
+++ calculater/claculater.js
@@ -12,8 +12,13 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
 })
 app.post('/',function(req,res){
-    console.log(req.body)
-    res.send("Thanks for tuppppp!")
+    // console.log(req.body)
+var num1 = req.body.num1;
+var num2 = req.body.num2;
+
+var result = number(num1)+  number(num2);
+
+res.send("Result is: " + result)
 })
 
 app.listen(3000,function(){
