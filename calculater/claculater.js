09961f2f65701536614443d5b998e2862f0ed719
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-19 07:02:41 +0430

    load_to file body_parser

--- calculater/claculater.js
+++ calculater/claculater.js
@@ -1,7 +1,11 @@
 // const {req,res} = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
 app.get('/', function(req, res){
     // res.send('<h1>Hello World</h1>');
