ae6d58a388b90d2a9587dc3899e4b28d64f97513
Hasibullah Aman <hasib.aman7@gmail.com>
2023-02-19 08:11:13 +0430

    coment_and_crete_newServer

--- calculater/claculater.js
+++ calculater/claculater.js
@@ -1,24 +1,24 @@
-// const {req,res} = require('express');
-const express = require('express');
-const bodyParser = require('body-parser');
+// // const {req,res} = require('express');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+// const app = express();
+// app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', function(req, res){
-    // res.send('<h1>Hello World</h1>');
-    res.sendFile(__dirname + '/index.html')
-})
-app.post('/',function(req,res){
-    // console.log(req.body)
-var num1 = Number(req.body.num1);
-var num2 = Number(req.body.num2);
+// app.get('/', function(req, res){
+//     // res.send('<h1>Hello World</h1>');
+//     res.sendFile(__dirname + '/index.html')
+// })
+// app.post('/',function(req,res){
+//     // console.log(req.body)
+// var num1 = Number(req.body.num1);
+// var num2 = Number(req.body.num2);
 
-var result = num1 + num2;
+// var result = num1 + num2;
 
-res.send("Result is: " + result);
-})
+// res.send("Result is: " + result);
+// })
 
-app.listen(3000,function(){
-    console.log('Express server listening on port:3000')
-})
+// app.listen(3000,function(){
+//     console.log('Express server listening on port:3000')
+// })
